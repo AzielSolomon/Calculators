afff22245b23350b289eabf1e9f5110131bcc9f8
Aziel Solomon <71221226+AzielSolomon@users.noreply.github.com>
2020-12-09 20:50:41 -0600

    Update normalcalculator.py

--- normalcalculator.py
+++ normalcalculator.py
@@ -1,38 +1,50 @@
-Intro = "This is a calculator that calculates Addition Mutlplication and Subtration and Divivsion"
-Example = "For the first question you could put 1, so that means you are doing addition and for the second question put 6 for the third question put 10 and you will get an output of 16"
-print (Intro)
-print (Example)
-def add(x, y):
-    return x + y
+def restart():
+    Intro = "This is a calculator that calculates Addition Mutlplication and Subtration and Divivsion"
+    Example = "For the first question you could put 1, so that means you are doing addition and for the second question put 6 for the third question put 10 and you will get an output of 16"
+    print (Intro)
+    print (Example)
+    def add(x, y):
+        return x + y
 
-def subtract(x, y):
-    return x - y
+    def subtract(x, y):
+        return x - y
 
-def multiply(x, y):
-    return x * y
+    def multiply(x, y):
+        return x * y
 
-def divide(x, y):
-    return x / y
+    def divide(x, y):
+        return x / y
 
 
-print("Chose the operation you want to do")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+    print("Chose the operation you want to do")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    while True:
+        choice = input("Enter choice(1/2/3/4): ")
 
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+            break
+        else:
+            print("Enter a valid number or READ example AND intro")
+            restart()
+restart()
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
